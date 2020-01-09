ab963a2d8dfcd71e8efef9200beb53800f2fb5c5
sohyeon <shana.cheon@betree.co.kr>
2020-01-09 16:29:41 +0900

    중간저자앙~ 집에가고싶다 ~~ 퇴근퇴근~~~ 한시간반이나 남았다니

--- slick/js/common.js
+++ slick/js/common.js
@@ -4,8 +4,25 @@ hoseo.faculty = window.hoseo.faculty || {};
 hoseo.faculty = (function ($) {
     var common = {
 
-        
+        slide: function(element) {
+            var $layer = $(element);
+            var $slider = $layer.find('.slider');
+            if($slider.length > 0) {
+                   $slider.each(function() {
+                        $(this).slick({
+                            dots:false,
+                            speed:250,
+                            infinite:true
+                        })
+                   });
+            }
+        },
+        slideTab : function() {
+
+        },
         init : function(){
+            common.slide('#container');
+            common.slideTab();
         }
     }
     $(document).ready(function () {
