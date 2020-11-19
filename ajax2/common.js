44c3f5960bb94d1c121ca2576be33d9c3e591c80
sohyeon <shana.cheon@betree.co.kr>
2020-11-19 16:13:05 +0900

    slick

--- ajax2/common.js
+++ ajax2/common.js
@@ -6,8 +6,15 @@
         success: function(data) {
             console.log('data',data);
             console.log('data',data.data[0].img);
-            // var src = $(data).find('img');
-            $('img').attr('src', data.data[0].img)
+            console.log(`data길이 ${data.data.length}`);
+
+            for (i = 0; i < data.data.length; i++) {
+                
+                var img_ = '<li><img src="' + data.data[i].img + '" alt="' + data.data[i].text + '"></li>';
+                console.log(`img_ ${img_}`);
+
+                ($('#wrapper')).append(img_);
+            }
         },
         error: err => console.log(err)
     });
--- slick/js/common.js
+++ slick/js/common.js
@@ -1,102 +1,74 @@
-var hoseo = window.hoseo || {};
+var hoseo = window.hoeso || {};
 hoseo.faculty = window.hoseo.faculty || {};
 
-hoseo.faculty = (function ($) {
+hoseo.faculty = (function($) {
     var common = {
 
-        slide: function(element) {
-            var $layer = $(element);
+        slide : function(el) {
+            var $layer = $(el);
             var $slider = $layer.find('.slider');
+
             if($slider.length > 0) {
-                   $slider.each(function() {
-                        $(this).slick({
-                            dots:false,
-                            speed:250,
-                            infinite:true
-                        })
-                   });
+                $slider.each(function() {
+                    $(this).slick({
+                        dots:false,
+                        speed:250,
+                        infinite:true
+                    })
+                })
             }
         },
-        //slider와 썸네일 이미지 연결하는 slick method는 'slickGoTo'!!!!!!!.
-        slideTab : function(element) {
-            var $layer = $(element);
+        //slider와 썸네일 이미지 연결하는 slick method는 'slickGoTo'.
+        slideTab: function(el) {
+            var $layer = $(el);
             var $slideTab = $layer.find('.slider_tab_wrap .slide_tab');
             $slideTab.on('click', function() {
-                var $slickSlide = $layer.find('.slider .slick-slide');
+                var $slickSlide = $layer.find('.slider.slick-slider');
                 if(!$slickSlide.is(':animated')) {
+                    console.log('!animated');
                     var slideIdx = $(this).index();
+                    console.log(`this(tab)의 인덱스는 ${slideIdx} 번 입니다`);
                     $(this).siblings().removeClass('on');
                     $(this).addClass('on');
-                    $(this).closest('.slide_wrap').find('.slider').slick('slickGoTo', slideIdx);
-
-                }
-            });
-        },
-        //slider 이동에 따른 썸네일 on효과 , 이미지 연동
-        slideTabSet : function(element) {
-            var $layer = $(element);
-            var $slider = $layer.find('.slider');
-            $slider.on("beforeChange", function(evet,slick,currentSlide,nextSlide) {
-                if(!$slider.find('.slick-slide').is(':animated')) {
-                    $(this).siblings('.slider_tab_wrap').find('.slide_tab').removeClass('on');
-                    $(this).siblings('.slider_tab_wrap').find('.slide_tab').eq(nextSlide).addClass('on');
-                    common.thumbnailSet();
-                }
-            });
-
-        },
-        //썸네일 호버시 data-img 변경
-        thumbnailSet : function() {
-            $('.slider_tab_wrap .slide_tab.').each(function() {
-                var onSrc = $(this).find('img').attr("data-on"),
-                    offSrc = $(this).find('img').attr("data-off") ;
-                if($(this).hasClass('on')) {
-                    $(this).find('img').attr('src', onSrc)
-                } else {
-                    $(this).find('img').attr('src', offSrc)
+                    $(this).closest('#container').find('.slider').slick('slickGoTo', slideIdx);
                 }
             })
         },
-        
-        // facultySlideTabHoverOn : function(){
-        //     $(".faculty .slider_tab_wrap .slide_tab button").on({
-        //         mouseenter: function () {
-        //             if(!$(this).parent().hasClass('on')){
-        //                 var onSrc = $(this).find('img').attr("data-on");
-        //                 $(this).find('img').attr('src', onSrc);
-        //                 $(this).parent().addClass('active');
-        //             }
-        //         },
-        //         mouseleave: function () {
-        //             if(!$(this).parent().hasClass('on')){
-        //                 $(this).parent().removeClass('active');
-        //                 var offSrc = $(this).find('img').attr("data-off");
-        //                 $(this).find('img').attr('src', offSrc);
-        //             }
-        //         }
-        //     });
-        // },
-        // facultySlideTabHoverOff : function(){
-        //     $(".faculty .slider_tab_wrap .slide_tab").off('mouseenter mouseleave');
-        // },
-        init : function(){
+        //slider 이동에 따른 썸네일 on효과 및 이미지 연동
+        slideTabSet: function(el) {
+            var $layer = $(el);
+            var $slider = $layer.find('.slider');
+            $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+                if(!$slider.is(':animated')) {
+                    $(this).parent().siblings('.slider_tab_wrap').find('.slide_tab').removeClass('on');
+                    $(this).parent().siblings('.slider_tab_wrap').find('.slide_tab').eq(nextSlide).addClass('on');
+                    common.thumbnailSet();
+                }
+            })
+        },
+        //썸네일 호버에따른 data-img 변경
+        thumbnailSet : function() {
+            $('.slider_tab_wrap .slide_tab').each(function() {
+                var onSrc = $(this).find('img').attr('data-on'),
+                    offSrc = $(this).find('img').attr('data-off');
+                if($(this).hasClass('on')) {
+                    $(this).find('img').attr('src', onSrc);
+                } else {
+                    $(this).find('img').attr('src', offSrc);
+                }
+            });
+        },
+
+        init : function() {
             common.slide('#container');
             common.slideTab('#container');
             common.slideTabSet('#container');
         }
     }
-    $(document).ready(function () {
+    $(document).ready(function() {
         common.init();
-        // if($("#wrapper").hasClass("major")) {
-        //     main.init();
-        // }
-        // if($("#wrapper").hasClass("sub")) {
-        //     sub.init();
-        // }
     });
-    return{
-        // openLayer : common.openLayer,
-        // closeLayer : common.closeLayer,
-        // galleryArray :sub.galleryArray,
+    return {
+
     }
 })(jQuery);
